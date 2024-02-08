6740517434563cb66aa85ba5446a8ca96ead853c
siraom15 <siriwatjaiyungyuen@gmail.com>
2024-02-09 05:39:29 +0700

    add

--- auto-update-remove-data.sh
+++ auto-update-remove-data.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
 cd rms
+
 pwd
+
 cd RMS-scripts
 pwd
+
 ./update.sh
 echo "Updated all"
+
 cd ..
 pwd
+
 cd RMS-compose
 pwd
+
+echo "compose down"
+docker compose -f docker-compose.dev.yml down
+
+echo "Removing data inside RMS-db"
+rm -rf ../RMS-db/data/*
+
 echo "building docker compose"
 docker compose -f docker-compose.dev.yml up -d --build
+
 cd ~
 pwd
